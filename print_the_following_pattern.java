6bef1fd53da39e4a9690f2a7e10fb604360fe621
D.saipavan <102216459+Dsaipavan@users.noreply.github.com>
2023-04-25 12:08:54 +0530

    import java.util.*; public class Main{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=0;i<n;i++){             for(int j=0;j<n;j++){                 if(i==j){                     System.out.print("0");                 }                 else{                     System.out.print("x");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,13 +1,18 @@
 import java.util.*;
-public class ABC {
-    public static void main (String[] args) {
+public class Main{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=0;i<n;i++){
-            for(int j=n;j>0;j--){
-                System.out.print(j+" ");
-        }
-        System.out.println();
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    System.out.print("0");
+                }
+                else{
+                    System.out.print("x");
+                }
+            }
+            System.out.println();
         }
     }
 }
